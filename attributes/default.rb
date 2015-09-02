9db1e045116012c1a47b5e7b2d178a875d6090d2
Xabier de Zuazo <xabier@zuazo.org>
2015-09-02 11:37:33 +0200

    Simplify platform attributes case statement

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,38 +19,32 @@
 # limitations under the License.
 #
 
-case node['platform']
-when 'windows'
-  default['ssl_certificate']['user'] = 'SYSTEM'
-else
-  default['ssl_certificate']['user'] = 'root'
-end
-
 default['ssl_certificate']['items'] = []
-
-case node['platform']
-when 'openbsd', 'freebsd', 'mac_os_x'
-  default['ssl_certificate']['group'] = 'wheel'
-when 'windows'
-  default['ssl_certificate']['group'] = 'Administrators'
-else
-  default['ssl_certificate']['group'] = 'root'
-end
 
 case node['platform_family']
 when 'debian'
+  default['ssl_certificate']['user'] = 'root'
+  default['ssl_certificate']['group'] = 'root'
   default['ssl_certificate']['key_dir'] = '/etc/ssl/private'
   default['ssl_certificate']['cert_dir'] = '/etc/ssl/certs'
 when 'rhel', 'fedora'
+  default['ssl_certificate']['user'] = 'root'
+  default['ssl_certificate']['group'] = 'root'
   default['ssl_certificate']['key_dir'] = '/etc/pki/tls/private'
   default['ssl_certificate']['cert_dir'] = '/etc/pki/tls/certs'
 when 'openbsd', 'freebsd', 'mac_os_x'
+  default['ssl_certificate']['user'] = 'root'
+  default['ssl_certificate']['group'] = 'wheel'
   default['ssl_certificate']['key_dir'] = '/etc/ssl'
   default['ssl_certificate']['cert_dir'] = '/etc/ssl'
 when 'windows'
+  default['ssl_certificate']['user'] = 'SYSTEM'
+  default['ssl_certificate']['group'] = 'Administrators'
   default['ssl_certificate']['key_dir'] = Chef::Config[:file_cache_path]
   default['ssl_certificate']['cert_dir'] = Chef::Config[:file_cache_path]
 else
+  default['ssl_certificate']['user'] = 'root'
+  default['ssl_certificate']['group'] = 'root'
   default['ssl_certificate']['key_dir'] = '/etc'
   default['ssl_certificate']['cert_dir'] = '/etc'
 end
