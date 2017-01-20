3df2745ecc843b6d2fcd10bd2e7b8ef89584e320
Andrew Brown <andrew.brown@shopify.com>
2017-01-20 13:22:29 -0500

    Fix cops

--- libraries/resource_ssl_certificate_pkcs12.rb
+++ libraries/resource_ssl_certificate_pkcs12.rb
@@ -56,8 +56,11 @@ class Chef
         def generate_pkcs12
           key = OpenSSL::PKey.read(key_content)
           crt = OpenSSL::X509::Certificate.new(cert_content)
-          chain = chain_content ? [crt, OpenSSL::X509::Certificate.new(chain_content)] : nil
-          OpenSSL::PKCS12.create(pkcs12_passphrase, name, key, crt, chain).to_der
+          chain = if chain_content
+                    [crt, OpenSSL::X509::Certificate.new(chain_content)]
+                  end
+          OpenSSL::PKCS12.create(pkcs12_passphrase,
+                                 name, key, crt, chain).to_der
         end
 
         def pkcs12_content
